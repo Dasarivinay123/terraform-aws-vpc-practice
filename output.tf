8c850b764c1bb9c3a96c3cdda6cd415fa64e2156
Vinay Dasari <dasariv211@gmail.com>
2026-09-21 15:51:50 +0530

    terraform

--- output.tf
+++ output.tf
@@ -9,13 +9,13 @@ output "vpc_id" {
 }
 output "public_subnet_ids" {
   description = "The IDs of the public subnets"
-  value       = aws_subnet.public.[*].id
+  value       = aws_subnet.public[*].id
 }
 output "private_subnet_ids" {
   description = "The IDs of the private subnets"
-  value       = aws_subnet.private.[*].id
+  value       = aws_subnet.private[*].id
 }
 output "database_subnet_id" {
   description = "The ID of the database subnet"
-  value       = aws_subnet.database.[*].id
+  value       = aws_subnet.database[*].id
 }
